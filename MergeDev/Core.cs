9192c6cce2c95ed5f3426dfcdb5223be762b5cfb
Johan van Rhyn <johanv@brightrock.co.za>
2019-11-14 23:43:37 +0200

    Fixed an unexpected recursion

--- MergeDev/Core.cs
+++ MergeDev/Core.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Linq;
 
 namespace MergeDev
 {
     public static class Core
     {
-        public static string Reverse(this string value)
+        public static string DoReverse(this string value)
         {
             // As ar as code goes, this is pointless
             // but we are testing github actions here.
-            return value.Reverse();
+            return value.Reverse().ToString();
         }
     }
 }
